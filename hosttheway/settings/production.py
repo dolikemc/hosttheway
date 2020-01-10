b47e3a7f7071d7c077cdc89136defc7251eb5c6c
chris_mit <chr@buchetmann.de>
2020-01-10 15:21:04 +0100

    production setup

--- hosttheway/settings/production.py
+++ hosttheway/settings/production.py
@@ -1,6 +1,28 @@
 from .base import *
 
 DEBUG = False
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'o=%z6r!m0u792slh3_cxt-ut6mf((^33n0*4-4%n5p55hfos2#'
+
+SECURE_CONTENT_TYPE_NOSNIFF = True
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['hosttheway.buchetmann.org']
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': '/var/www/ssd123/priv/hosttheway/db.conf',
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
+    }
+}
+
+STATIC_ROOT = '/var/www/ssd123/htdocs/hosttheway/static/'
+STATIC_URL = '/static/'
+
+MEDIA_ROOT = '/var/www/ssd1470/htdocs/hosttheway/media/'
+MEDIA_URL = '/media/'
 
 try:
     from .local import *
