d3344a1652b09d4cc191386236cebeca9984eb29
chris_mit <chr@buchetmann.de>
2020-03-12 13:33:00 +0100

    world boarders setting up gis

--- hosttheway/settings/dev.py
+++ hosttheway/settings/dev.py
@@ -12,10 +12,23 @@ ALLOWED_HOSTS = ['*']
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
+# you need to download the whl package from https://www.lfd.uci.edu/~gohlke/pythonlibs/#gdal
+# and install it via pip install GDAL-3.0.4-cp37-cp37m-win.whl
 if os.name in ('nt'):
     GDAL_LIBRARY_PATH = r'C:\Users\buchetmc\PycharmProjects\GIT\hosttheway\venv4\Lib\site-packages\osgeo\gdal300.dll'
+    #SPATIALITE_LIBRARY_PATH = r'mod_spatialite'
 else:
     GDAL_LIBRARY_PATH = ''
+
+# Database
+# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.spatialite',
+        'NAME': os.path.join(BASE_DIR, 'dev.sqlite3'),
+    }
+}
 
 try:
     from .local import *
